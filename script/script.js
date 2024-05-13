20f15bd94aec3758d91886c44ab2e379cdc1229c
Sabbirador <sabbirador69@gmail.com>
2024-05-14 02:15:23 +0600

    alphabet changes done

--- script/script.js
+++ script/script.js
@@ -4,4 +4,29 @@ function play(){
         
         const playGround=document.getElementById('play-ground');
         playGround.classList.remove('hidden');
+        continueGame();
+}
+
+function continueGame(){
+        const alphabet =getARandomAlphabet();
+        console.log('Your random alphabet is',alphabet);
+
+        const currentAlphabet =document.getElementById('current-alphabet');
+        currentAlphabet.innerText = alphabet ;
+}
+
+
+function getARandomAlphabet(){
+        const alphabetString='abcdefghijklmnopqrstuvwxyz';
+        const alphabets =alphabetString.split('');
+        
+
+        const randomNumber=Math.random()*25 ;
+        const index =Math.round(randomNumber);
+        console.log(index);
+
+        const alphabet=alphabets[index];
+        return alphabet;
+
+         
 }
